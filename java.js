d61eb3b295942bc6f9f206108a009e10a09c444a
justin <turveystaana681@gmail.com>
2023-11-28 09:38:00 -0700

    styles

---
+++ java.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+// script.js
+document.getElementById("pageTitle").innerHTML = "Hello, Universe!";
+document.getElementById("introText").innerHTML = "This is my awesome webpage!";
